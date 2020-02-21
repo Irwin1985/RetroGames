3cccab2a80fbe16e5100b863ac10e341958f2468
LDF <damianldf@yahoo.com.ar>
2020-02-20 21:33:52 -0300

    Hi-scores separados por modo de juego y nivel (en challenge mode), y guardados en json

--- Circus Charlie/HUD/LabelPause.gd
+++ Circus Charlie/HUD/LabelPause.gd
@@ -32,6 +32,7 @@ func _input(event):
 				get_tree().paused = true
 			else:
 				get_tree().paused = false
+				global.check_update_hi_score()
 				global.save_game()
 				get_tree().call_deferred("change_scene", "res://Menu/Menu.tscn")
 
